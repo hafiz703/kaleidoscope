2c3ab49d34848df4f14ca1ee8f6026e725912bc7
hafiz703 <mhbt-703@hotmail.com>
2018-01-11 10:41:50 +0800

    Added hud and clamping

---
+++ examples/vid.js
@@ -0,0 +1,12 @@
+// var containerSelector = '#container360';
+// var viewer = new Kaleidoscope.Video({
+//     source: 'http://thiago.me/demo-360/cool.mp4',
+//     containerId: containerSelector,
+//     height: window.innerHeight,
+//     width: window.innerWidth
+// });
+// viewer.render();
+// window.onresize = function() {
+//     viewer.setSize({height: window.innerHeight, width: window.innerWidth});
+// };
+// document.querySelector(containerSelector).addEventListener('touchend', viewer.play.bind(viewer));
